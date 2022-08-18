3c91399f8962ddf567913e024004149bed18be81
guifwiener <gfw1994@gmail.com>
2022-08-18 10:38:19 -0300

    End Sec 19 - No express not complete

--- Sec19/calculatorExpress/calculator.js
+++ Sec19/calculatorExpress/calculator.js
@@ -12,9 +12,23 @@ app.post("/", (req, res) => {
   var num1 = parseInt(req.body.num1);
   var num2 = parseInt(req.body.num2);
   var sum = num1 + num2;
-
   res.send(`The sum of the numbers is ${sum}`);
 });
+
+
+//BMI calculator
+var bmiRoute = "/bmicalculator/";
+
+app.get(bmiRoute, (req,res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post(bmiRoute, (req,res) => {
+  var height = parseFloat(req.body.height);
+  var weight = parseFloat(req.body.weight);
+  var bmi = weight / (height*height);
+  res.send(`Your BMI is ${bmi}`);
+})
 
 app.listen(3000, () => {
   console.log(`Listening on port ${port}`);
--- Sec19/calculatorOnlyNode/calculator-copy.js
+++ Sec19/calculatorOnlyNode/calculator-copy.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const hostname = "127.0.0.1";
 const port = 3000;
 
+
+
+
 const server = http
     .createServer((req,res) => {
         fs.readFile('./index.html', (err,html) => {
